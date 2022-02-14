ec67c1503e9d81e799327b5272ea890f6b48ef9c
klmhyeonwooo <gentlemonster77@naver.com>
2022-02-14 16:01:17 +0900

    chapter 06 new

---
+++ chapter06/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import youtubeData from './data/youtubeData.json';
+
+
+function App() {
+  console.log(youtubeData );
+
+  return (
+    <div>
+      <p> data-test </p>
+    </div>
+  );
+}
+
+export default App;
